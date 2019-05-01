7b98005be038b3fe64686b9b541b362548abab93
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-05-01 18:42:03 +0100

    exercise done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json'
+import jsonContacts from './contacts'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state ={
+      contacts: jsonContacts.slice(0,5)
+
+    }
+    this.AddContact = this.AddContact.bind(this)
+    this.SortContact = this.SortContact.bind(this)
+    this.SortPopularity = this.SortPopularity.bind(this)
+
+  }
+    AddContact() {
+      let randomIndex = Math.floor(Math.random() * jsonContacts.length)
+      let randomContact = jsonContacts[randomIndex]
+    
+      this.setState ({
+        contacts: [...this.state.contacts, randomContact]
+      })
+    }
+    SortContact() {
+      let newContacts = [...this.state.contacts].sort((a,b )=> {
+        if(a.name < b.name) return -1
+        else return 1
+      })
+      this.setState ({
+        contacts: newContacts
+      })
+  }
+  SortPopularity() {
+    let sortedContact = [...this.state.contacts].sort((a,b) => {
+      if(a.popularity < b.popularity) return -1
+      else return 1
+    })
+
+    this.setState ({
+      contacts: sortedContact
+    })
+}
+
+
+
   render() {
     return (
       <div className="App">
       <h1>IronContacts</h1>
+      <button onClick = {this.AddContact}> Add Random Contact</button>
+      <button onClick = {this.SortContact}>Sort by name</button>
+      <button onClick = {this.SortPopularity}> Sort by popularity</button>
+
+
       <table>
           <thead>
             <tr>
@@ -18,11 +64,11 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-          {contacts.map((contacts, i) => (
+          {this.state.contacts.map((contact,i) => (
               <tr>
-                <td><img src={contacts.pictureUrl} alt="" height="100" width="70"/></td>
-                <td>{contacts.name}</td>
-                <td>{contacts.popularity}</td>
+                <td><img src={contact.pictureUrl} alt="" height="60" /></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
               </tr>
             ))}
           </tbody>
